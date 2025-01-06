cf878e96636fad6b4f6a130997a775bcb3230e55
Lorenzo Carbonell a.k.a. atareao <lorenzo.carbonell.cerezo@gmail.com>
2025-01-06 10:38:21 +0100

    :sparkles: Añadida nueva funcionalidad de autenticación de usuarios

---
+++ src/models/site.rs
@@ -0,0 +1,25 @@
+use serde::{Serialize, Deserialize};
+use super::{Telegram, Mastodon};
+use std::fmt::{self, Display};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Site{
+    pub url: String,
+    pub title: String,
+    pub description: String,
+    pub author: String,
+    pub avatar: String,
+    pub email: String,
+    pub telegram: Option<Telegram>,
+    pub mastodon: Option<Mastodon>,
+}
+
+impl Display for Site{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "url: {}\title: {}\ndescription: {}",
+            self.url,
+            self.title,
+            self.description,
+        )
+    }
+}
